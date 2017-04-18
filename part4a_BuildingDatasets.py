9e203150cbda1e91eaea5805b1dbd23abe80aa00
SebasVM <sebastiaan.westdijk@eneco.com>
2017-04-18 17:03:43 +0200

    Build list of US states from different source

--- part4a_BuildingDatasets.py
+++ part4a_BuildingDatasets.py
@@ -1,1 +1,26 @@
-part4a_BuildingDatasets.py
+import quandl
+import pandas as pd
+
+#Use HPI from Freddie Mac on Quandl
+api_key = 'cyJUASK__T4JA9s4T1JG'
+
+#read Quandl API to get HPI from Alaska
+df = quandl.get('FMAC/HPI_AK', authtoken=api_key)
+print(df.head())
+
+#get 50 states from Wikipedia
+fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
+
+#this is a list:
+print(fiddy_states)
+
+#this is a dataframe:
+print(fiddy_states[0])
+
+#this is a column:
+print(fiddy_states[0][0])
+
+for abbv in fiddy_states[0][0][1:]:
+	print("FMAC/HPI_"+str(abbv))
+
+#get 50 states list from webpage other than Wikipedia
